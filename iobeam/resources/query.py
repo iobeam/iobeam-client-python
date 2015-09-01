1b555b6bd497c74fe6a9086720f1bec658080d04
Rob Kiefer <rob.kiefer@gmail.com>
2015-09-01 12:24:48 -0400

    Query params set with None should be no-ops; tests for resources/query.

--- iobeam/resources/query.py
+++ iobeam/resources/query.py
@@ -31,8 +31,11 @@ class Query(object):
     Sets the limit of this query, i.e., the max number of results series.
     '''
     def limit(self, limit):
-        if not isinstance(limit, int):
-            raise ValueError("limit must be an int")
+        if limit is None:
+            return self
+        if not isinstance(limit, int) or not limit > 0:
+            raise ValueError("limit must be a positive int")
+
         self._params["limit"] = limit
         return self
 
@@ -40,8 +43,11 @@ class Query(object):
     Sets the from time limit for results.
     '''
     def fromTime(self, time):
-        if not isinstance(time, int):
+        if time is None:
+            return self
+        elif not isinstance(time, int):
             raise ValueError("time must be an int (milliseconds from epoch)")
+
         self._params["from"] = time
         return self
 
@@ -49,8 +55,11 @@ class Query(object):
     Sets the to time limit for results.
     '''
     def toTime(self, time):
-        if not isinstance(time, int):
+        if time is None:
+            return self
+        elif not isinstance(time, int):
             raise ValueError("time must be an int (milliseconds from epoch)")
+
         self._params["to"] = time
         return self
 
@@ -58,8 +67,11 @@ class Query(object):
     Sets the max value for values in the results.
     '''
     def lessThan(self, value):
-        if not isinstance(value, int):
+        if value is None:
+            return self
+        elif not isinstance(value, int):
             raise ValueError("value must be an int")
+
         self._params["less_than"] = value
         return self
 
@@ -67,8 +79,11 @@ class Query(object):
     Sets the min value for values in the results.
     '''
     def greaterThan(self, value):
-        if not isinstance(value, int):
+        if value is None:
+            return self
+        elif not isinstance(value, int):
             raise ValueError("value must be an int")
+
         self._params["greater_than"] = value
         return self
 
@@ -76,7 +91,10 @@ class Query(object):
     Sets the value that all results must equal.
     '''
     def equals(self, value):
-        if not isinstance(value, int):
+        if value is None:
+            return self
+        elif not isinstance(value, int):
             raise ValueError("value must be an int")
+
         self._params["equals"] = value
         return self
---
+++ tests/test_query.py
@@ -0,0 +1,182 @@
+import unittest
+
+from iobeam.resources import query
+
+_PROJECT_ID = 1
+_DEVICE_ID = "py_test_id"
+_SERIES = "temp"
+
+
+class TestQuery(unittest.TestCase):
+
+    def test_constructorOnlyProject(self):
+        q = query.Query(_PROJECT_ID)
+        self.assertEqual(_PROJECT_ID, q._pid)
+        self.assertTrue(q._did is None)
+        self.assertTrue(q._series is None)
+        self.assertEqual(0, len(q._params))
+
+    def test_invalidProjectId(self):
+        # None is not a valid project id
+        badId = None
+        try:
+            q = query.Query(badId)
+            self.assertTrue(False)
+        except ValueError as e:
+            self.assertEqual("projectId must be an int", str(e))
+
+        # No non-int project ids
+        badId = "50"
+        try:
+            q = query.Query(badId)
+            self.assertTrue(False)
+        except ValueError as e:
+            self.assertEqual("projectId must be an int", str(e))
+
+        # No negative project ids
+        badId = -1
+        try:
+            q = query.Query(badId)
+            self.assertTrue(False)
+        except ValueError as e:
+            self.assertEqual("projectId must be greater than 0", str(e))
+
+        # 0 is not a valid project id
+        badId = 0
+        try:
+            q = query.Query(badId)
+            self.assertTrue(False)
+        except ValueError as e:
+            self.assertEqual("projectId must be greater than 0", str(e))
+
+    def test_getUrl(self):
+        ALL = "all"
+        q = query.Query(_PROJECT_ID)
+        self.assertEqual("{}/{}/{}".format(_PROJECT_ID, ALL, ALL), q.getUrl())
+
+        q = query.Query(_PROJECT_ID, deviceId=_DEVICE_ID)
+        self.assertEqual("{}/{}/{}".format(_PROJECT_ID, _DEVICE_ID, ALL),
+                         q.getUrl())
+
+        q = query.Query(_PROJECT_ID, seriesName=_SERIES)
+        self.assertEqual("{}/{}/{}".format(_PROJECT_ID, ALL, _SERIES),
+                         q.getUrl())
+
+        q = query.Query(_PROJECT_ID, deviceId=_DEVICE_ID, seriesName=_SERIES)
+        self.assertEqual("{}/{}/{}".format(_PROJECT_ID, _DEVICE_ID, _SERIES),
+                         q.getUrl())
+
+    # None should be essentially a no-op
+    def paramNoneTest(self, q, func):
+        ret = func(None)
+        self.assertEqual(0, len(q.getParams()))
+        self.assertEqual(q, ret)
+
+    def test_limit(self):
+        q = query.Query(_PROJECT_ID)
+
+        self.paramNoneTest(q, q.limit)
+
+        ret = q.limit(5)
+        self.assertEqual(5, q.getParams()["limit"])
+        self.assertEqual(q, ret)
+
+        # Failure case: non-int
+        try:
+            q.limit("junk")
+            self.assertTrue(False)
+        except ValueError as e:
+            pass
+
+        # Failure cases: non-positive int
+        try:
+            q.limit(0)
+            self.assertTrue(False)
+        except ValueError as e:
+            pass
+
+        try:
+            q.limit(-1)
+            self.assertTrue(False)
+        except ValueError as e:
+            pass
+
+    def test_fromTime(self):
+        q = query.Query(_PROJECT_ID)
+
+        self.paramNoneTest(q, q.fromTime)
+
+        ret = q.fromTime(5000)
+        self.assertEqual(5000, q.getParams()["from"])
+        self.assertEqual(q, ret)
+
+        # Failure case: non-int
+        try:
+            q.fromTime("junk")
+            self.assertTrue(False)
+        except ValueError as e:
+            pass
+
+    def test_toTime(self):
+        q = query.Query(_PROJECT_ID)
+
+        self.paramNoneTest(q, q.toTime)
+
+        ret = q.toTime(15000)
+        self.assertEqual(15000, q.getParams()["to"])
+        self.assertEqual(q, ret)
+
+        # Failure case: non-int
+        try:
+            q.toTime("junk")
+            self.assertTrue(False)
+        except ValueError as e:
+            pass
+
+    def test_greaterThan(self):
+        q = query.Query(_PROJECT_ID)
+
+        self.paramNoneTest(q, q.greaterThan)
+
+        ret = q.greaterThan(0)
+        self.assertEqual(0, q.getParams()["greater_than"])
+        self.assertEqual(q, ret)
+
+        # Failure case: non-int
+        try:
+            q.greaterThan("junk")
+            self.assertTrue(False)
+        except ValueError as e:
+            pass
+
+    def test_lessThan(self):
+        q = query.Query(_PROJECT_ID)
+
+        self.paramNoneTest(q, q.lessThan)
+
+        ret = q.lessThan(1000)
+        self.assertEqual(1000, q.getParams()["less_than"])
+        self.assertEqual(q, ret)
+
+        # Failure case: non-int
+        try:
+            q.greaterThan("junk")
+            self.assertTrue(False)
+        except ValueError as e:
+            pass
+
+    def test_equals(self):
+        q = query.Query(_PROJECT_ID)
+
+        self.paramNoneTest(q, q.equals)
+
+        ret = q.equals(10)
+        self.assertEqual(10, q.getParams()["equals"])
+        self.assertEqual(q, ret)
+
+        # Failure case: non-int
+        try:
+            q.greaterThan("junk")
+            self.assertTrue(False)
+        except ValueError as e:
+            pass
