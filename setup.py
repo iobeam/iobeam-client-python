d677414e0c3e10e1404d15076c60cf1daa6fa7bc
Rob Kiefer <rob.kiefer@gmail.com>
2015-08-31 20:48:19 -0400

    Tweaks to setup

--- setup.py
+++ setup.py
@@ -11,11 +11,11 @@ from setuptools import setup, find_packages
 from codecs import open
 from os import path
 
-#here = path.abspath(path.dirname(__file__))
+here = path.abspath(path.dirname(__file__))
 
 # Get the long description from the relevant file
-#with open(path.join(here, 'DESCRIPTION.rst'), encoding='utf-8') as f:
-#    long_description = f.read()
+with open(path.join(here, 'README.md'), encoding='utf-8') as f:
+    long_description = f.read()
 
 setup(
     name='iobeam',
@@ -23,12 +23,12 @@ setup(
     # Versions should comply with PEP440.  For a discussion on single-sourcing
     # the version across setup.py and the project code, see
     # https://packaging.python.org/en/latest/single_source_version.html
-    version='0.1.0',
+    version='0.1.2',
 
-    description='Library for connecting to the iobeam, the data analysis \
-    platform for the Internet of Things',
-    long_description='Library for connecting to the iobeam, the data analysis \
-    platform for the Internet of Things',
+    description='Library for connecting to iobeam, the data analysis \
+    platform for the Internet of Things.',
+    long_description='Library for connecting to iobeam, the data analysis\
+    platform for the Internet of Things.',
 
     # The project's main homepage.
     url='https://github.com/iobeam/iobeam-client-python',
@@ -57,6 +57,7 @@ setup(
 
         # Specify the Python versions you support here. In particular, ensure
         # that you indicate whether you support Python 2, Python 3 or both.
+        'Programming Language :: Python',
         'Programming Language :: Python :: 2.7',
         'Programming Language :: Python :: 3.4',
     ],
