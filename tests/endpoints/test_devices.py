70f3749cb4a2a28b2d6aaf470b384124fcbfc57b
Rob Kiefer <rob.kiefer@gmail.com>
2015-09-14 23:46:07 -0400

    endpoints/devices: expands testing coverage

--- tests/endpoints/test_devices.py
+++ tests/endpoints/test_devices.py
@@ -20,9 +20,7 @@ class DummyDeviceService(request.DummyRequest):
 
     def __init__(self):
         request.DummyRequest.__init__(self, None, None)
-        self.lastParams = None
-        self.lastHeaders = None
-        self.lastJson = None
+        self.reset()
 
     def dummyExecute(self, url, params=None, headers=None, json=None):
         class Resp(dict):
@@ -30,6 +28,11 @@ class DummyDeviceService(request.DummyRequest):
 
             def json(self):
                 return self
+
+        self.lastParams = params
+        self.lastHeaders = headers
+        self.lastJson = json
+        self.calls += 1
 
         if "devices/timestamp" in url:
             return Resp(self.getTimestamp())
@@ -52,6 +55,12 @@ class DummyDeviceService(request.DummyRequest):
         dname = deviceName or _NONE_DEVICE_NAME
         return {"status_code": 201, "device_id": did, "device_name": dname}
 
+    def reset(self):
+        self.lastParams = None
+        self.lastHeaders = None
+        self.lastJson = None
+        self.calls = 0
+
 class TestDeviceService(unittest.TestCase):
 
     def test_getTimestamp(self):
@@ -62,6 +71,7 @@ class TestDeviceService(unittest.TestCase):
 
         ret = service.getTimestamp()
         self.assertEqual(_TIMESTAMP, ret)
+        self.assertEqual(1, dummy.calls)
 
     def _checkDevice(self, ret, pid, did, dname):
         self.assertTrue(isinstance(ret, Device))
@@ -77,6 +87,7 @@ class TestDeviceService(unittest.TestCase):
 
         ret = service.registerDevice(1)
         self._checkDevice(ret, 1, _NONE_DEVICE_ID, _NONE_DEVICE_NAME)
+        self.assertEqual(1, dummy.calls)
 
     def test_registerDevice(self):
         dummy = DummyDeviceService()
@@ -86,10 +97,22 @@ class TestDeviceService(unittest.TestCase):
 
         ret = service.registerDevice(1, deviceId="a_given_id")
         self._checkDevice(ret, 1, "a_given_id", _NONE_DEVICE_NAME)
+        self.assertEqual(1, dummy.calls)
+        body = dummy.lastJson
+        self.assertEqual("a_given_id", body["device_id"])
 
         ret = service.registerDevice(
             1, deviceId="a_given_id2", deviceName="a_given_name")
         self._checkDevice(ret, 1, "a_given_id2", "a_given_name")
+        self.assertEqual(2, dummy.calls)
+        body = dummy.lastJson
+        self.assertEqual("a_given_id2", body["device_id"])
+        self.assertEqual("a_given_name", body["device_name"])
 
         ret = service.registerDevice(1, deviceName="a_given_name2")
         self._checkDevice(ret, 1, _NONE_DEVICE_ID, "a_given_name2")
+        self.assertEqual(3, dummy.calls)
+        body = dummy.lastJson
+        self.assertEqual("a_given_name2", body["device_name"])
+
+    # TODO test error conditions
