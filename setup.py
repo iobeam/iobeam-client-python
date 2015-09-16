215ccbfbebf058377eb89abc39ff752b8613b2d8
Rob Kiefer <rob.kiefer@gmail.com>
2015-09-16 15:00:27 -0400

    v0.4.0 ready

--- setup.py
+++ setup.py
@@ -16,7 +16,7 @@ setup(
     # Versions should comply with PEP440.  For a discussion on single-sourcing
     # the version across setup.py and the project code, see
     # https://packaging.python.org/en/latest/single_source_version.html
-    version='0.3.1',
+    version='0.4.0',
 
     description='Library for connecting to iobeam, the data analysis \
     platform for the Internet of Things.',
@@ -64,7 +64,7 @@ setup(
     # your project is installed. For an analysis of "install_requires" vs pip's
     # requirements files see:
     # https://packaging.python.org/en/latest/requirements.html
-    install_requires=['requests'],
+    install_requires=['requests', 'enum34'],
 
     # List additional groups of dependencies here (e.g. development
     # dependencies). You can install these using the following syntax,
