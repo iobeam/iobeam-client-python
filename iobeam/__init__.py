9a34566be997320296c7982cef28cae49af76268
Rob Kiefer <rob.kiefer@gmail.com>
2015-09-25 22:13:41 -0400

    main: Make import iobeam workable.

---
+++ iobeam/__init__.py
@@ -0,0 +1,9 @@
+import iobeam
+
+ClientBuilder = iobeam.ClientBuilder
+MakeQuery = iobeam.MakeQuery
+DataPoint = iobeam.DataPoint
+DataSeries = iobeam.DataSeries
+Timestamp = iobeam.Timestamp
+TimeUnit = iobeam.TimeUnit
+QueryReq = iobeam.QueryReq
