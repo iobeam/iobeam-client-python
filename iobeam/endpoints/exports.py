1661c078ceddddfb62c6f24dd295d90005f09752
Rob Kiefer <rob.kiefer@gmail.com>
2015-10-01 15:04:32 -0400

    endpoints: linting pass.

--- iobeam/endpoints/exports.py
+++ iobeam/endpoints/exports.py
@@ -1,32 +1,31 @@
+"""Used to communicate with iobeam's Exports API"""
 from iobeam.endpoints import service
 from iobeam.http import request
 
-'''
-Communicates with the backend and exposes available Exports API methods.
-'''
+
 class ExportService(service.EndpointService):
+    """Communicates with the backend and exposes available Exports API methods."""
 
     def __init__(self, token, requester=None):
         service.EndpointService.__init__(self, token, requester=requester)
 
-    '''
-    Wraps API call `POST /exports`
-
-    Queries data from the iobeam backend.
-
-    Params:
-        query - A iobeam.resources.Query object that contains the parameters
-                for the query.
-
-    Returns:
-        A dictionary representing the query results.
-
-    Raises:
-        Exception - If `query` is None
-    '''
     def getData(self, query):
+        """Wraps API call `POST /exports`
+
+        Queries data from the iobeam backend.
+
+        Params:
+            query - A iobeam.resources.Query object that contains the parameters
+                    for the query.
+
+        Returns:
+            A dictionary representing the query results.
+
+        Raises:
+            Exception - If `query` is None
+        """
         if not self.token:
-            raise request.UnauthorizedError.noTokenSet();
+            raise request.UnauthorizedError.noTokenSet()
         if query is None:
             raise Exception("query cannot be None")
 
